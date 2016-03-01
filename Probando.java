10b6e40eb4fcfba65bcb545d617b9207cd56dd5f
raulrod <raulrodriguezr@hotmail.com>
2016-03-01 11:45:43 +0100

    Added files via upload

---
+++ Probando.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probando;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author raulrod
+ */
+public class Probando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("hola mundo");
+    }
+    
+}
